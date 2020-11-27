01883513cf26db9bd846f4434c22cfaa08c2ec59
ishiningway <ishiningway@qq.com>
2020-11-27 22:57:56 +0800

    init

---
+++ src/main/java/com/github/ishiningway/bullshit/BullshitApplication.java
@@ -0,0 +1,13 @@
+package com.github.ishiningway.bullshit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullshitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullshitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ishiningway/bullshit/BullshitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ishiningway.bullshit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullshitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
